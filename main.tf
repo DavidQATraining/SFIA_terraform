a8cf2fb07bdfd445142a7aec08d5f913d23337b5
Ubuntu <ubuntu@ip-172-31-46-147.eu-west-1.compute.internal>
2020-08-19 14:31:19 +0000

    added seciruty group, internet gateaway, route table, and route table association

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 
 }
 
+# create VPC
 resource "aws_vpc" "main" {
   cidr_block           = var.cidr_block
   instance_tenancy     = "default"
@@ -14,23 +15,105 @@ resource "aws_vpc" "main" {
   }
 }
 
+# fetch me all teh availability zones for the region I'm in
 data "aws_availability_zones" "available" {
   state = "available"
 }
 
+# create the subnet based of created vpc
 resource "aws_subnet" "subnet_a" {
-  vpc_id             = aws_vpc.main.id
-  cidr_block         = var.sub_cidr_block
-  availability_zones = data.aws_availability_zones.available.names[0]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.sub_cidr_block
+  availability_zone = data.aws_availability_zones.available.names[0]
 
   tags = {
     Name = "Main"
   }
 }
 
+# ADD ROUTE TABLE ASSOCIATION > ROUTE TABLE > INTERNET GATEWAY. BELOW \/ 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# resource "aws_egress_only_internet_gateway" "main" {
+#   vpc_id = aws_vpc.main.id
+
+#   tags = {
+#     Name = "main"
+#   }
+# }
+
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+#   route {
+#     ipv6_cidr_block        = "::/0"
+#     egress_only_gateway_id = aws_egress_only_internet_gateway.main.id
+#   }
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet_a.id
+  route_table_id = aws_route_table.r.id
+}
+
+resource "aws_security_group" "ingress-all-test" {
+  name   = "allow-all-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+  }
+
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+  }
+
+  // Terraform removes the default rule
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
+# creates ec2 instance
 resource "aws_instance" "web" {
-  ami           = var.ami
-  instance_type = var.instance
-  key_name      = var.key_name
+  ami                         = var.ami
+  instance_type               = var.instance
+  key_name                    = var.key_name
+  subnet_id                   = aws_subnet.subnet_a.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["${aws_security_group.ingress-all-test.id}"]
+
 }
 
