fb30a8b19fdbd1f4297ec7d57957cf5f783fc5fc
Ubuntu <ubuntu@ip-172-31-46-147.eu-west-1.compute.internal>
2020-08-19 11:59:51 +0000

    added VPC and subnet terraform creation

--- main.tf
+++ main.tf
@@ -4,6 +4,30 @@ provider "aws" {
 
 }
 
+resource "aws_vpc" "main" {
+  cidr_block       = var.cidr_block
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "terraformVPC"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.sub_cidr_block
+  availability_zones = data.aws_availability_zones.available.names[]
+
+  tags = {
+    Name = "Main"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance
