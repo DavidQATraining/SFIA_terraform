f8f6add8467293b0c5a82a5809e087c1fcf2e401
Ubuntu <ubuntu@ip-172-31-46-147.eu-west-1.compute.internal>
2020-08-20 09:47:33 +0000

    creating modules

--- main.tf
+++ main.tf
@@ -1,14 +1,16 @@
 provider "aws" {
   region                  = var.region
   shared_credentials_file = "~/.aws/credentials"
-
 }
 
+module "VPC"{
+  source = "./VPC"
+}
 
+module "SG"{
+  source = "./SG"
+}
 
-
-
-
-
-
-
+module "EC2"{
+  source = "./EC2"
+}
